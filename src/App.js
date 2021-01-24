fe89ebe177a276d668e791356505648fbfef2707
Gevorg0013 <gevorg.ayvazyan.exp@gmail.com>
2021-01-24 11:44:47 -0800

    fix icon bug

--- src/App.js
+++ src/App.js
@@ -1,95 +1,75 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import SortByName from "./Component/TableHead";
 import DataTable from "./Component/DataTable";
-import TableHead from "./Component/TableHead";
 
-const App=()=> {
-   let usersObj=[
-        {name:"Gevorg",SurName:"Ayvazyan", age:24},
-        {name:"Aram",SurName:"Poxosyan", age:20},
-        {name:"Erik",SurName:"Xlxatyan", age:25},
-        {name:"Edgar",SurName:"Vardanyan", age:23},
-        {name:"Sergey",SurName:"Mxitaryan", age:22},
-        {name:"Aram",SurName:"Kirakosyan", age:26},
-    ]
+const App = () => {
+  let usersObj = [
+    { name: "Gevorg", SurName: "Ayvazyan", age: 24 },
+    { name: "Aram", SurName: "Poxosyan", age: 20 },
+    { name: "Erik", SurName: "Xlxatyan", age: 25 },
+    { name: "Edgar", SurName: "Vardanyan", age: 23 },
+    { name: "Sergey", SurName: "Mxitaryan", age: 22 },
+    { name: "Aram", SurName: "Kirakosyan", age: 26 },
+  ];
 
-     const [users,setUsers]=useState(usersObj);
-     let [filteredNames,setFilteredNames]=useState(users);
+  // eslint-disable-next-line no-unused-vars
+  const [users, setUsers] = useState(usersObj);
+  // eslint-disable-next-line no-unused-vars
+  const [users1, setUsers1] = useState(users);
+  let [filteredNames, setFilteredNames] = useState(users);
 
-     const filterUser=(event)=> {
+  const filterUser = (event) => {
+    let usersAsString = users.map((user) => {
+      return user.name
+        .toLowerCase()
+        .concat(" ")
+        .concat(user.SurName.toLowerCase())
+        .concat(" ")
+        .concat(user.age);
+    });
+    // let val = event.target.value;
 
-           let userCopy = [...users];
-           let val=event.target.value;
-           let userArray = []
+    const valueAsArray = event.target.value.toLowerCase().split(" ");
+    valueAsArray.map((str) => {
+      usersAsString = usersAsString.filter((userString) => {
+        return userString.includes(str);
+      });
+    });
 
-           const valueAsArray = val.toLowerCase().split(" ");
+    const userArray = usersAsString.map((item) => {
+      let userInfo = item.split(" ");
+      return {
+        name: userInfo[0].charAt(0).toUpperCase().concat(userInfo[0].slice(1)),
+        SurName: userInfo[1]
+          .charAt(0)
+          .toUpperCase()
+          .concat(userInfo[1].slice(1)),
+        age: userInfo[2],
+      };
+    });
+    setFilteredNames(userArray);
+  };
 
-           valueAsArray.forEach((str) => {
-               userArray = userCopy.filter((user) => {
-                   let userDataString = user.name.toLowerCase().concat(" ")
-                       .concat(user.SurName.toLowerCase()).concat(" ")
-                       .concat(user.age);
-                   return userDataString.includes(str);
-               });
-           });
+  return (
+    <body>
+      <div className="App">
+        <div className="column">
+          <div className="inputDiv">
+            <input type="text" placeholder="filter" onChange={filterUser} />
+          </div>
+          <div className="imagesDiv">
+            <div className="divList">
+              <DataTable
+                className="ListUl"
+                filteredNames={filteredNames}
+                setFilteredNames={setFilteredNames}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
 
-           setFilteredNames(userArray);
-       }
-
-         const refName=React.createRef();
-         const  refSurName=React.createRef();
-         const  refAge=React.createRef();
-
-    const addEl=()=>{
-        const rName=refName.current.value;
-        const rSurName=refSurName.current.value;
-        const rAge=refAge.current.value;
-        if(rAge===""||rSurName===""||rName===""){
-            alert("try again")}
-        else {
-            setUsers((current) => [...current, {name: rName, SurName: rSurName, age: rAge}]);
-            setFilteredNames((current) => [...current, {name: rName, SurName: rSurName, age: rAge},]);
-        }
-    }
-
-
-
-        return(
-            <body>
-             <div className="App">
-                 <div className="column">
-                     <div className="inputDiv">
-                         <input type="text" placeholder="filter" onChange={filterUser}/>
-                     </div>
-                     <div className="imagesDiv">
-                         <div className="divList">
-                             <DataTable className="ListUl" filteredNames={filteredNames} setFilteredNames={setFilteredNames}/>
-                         </div>
-
-                         <div className="dd">
-                             <input type="text" placeholder="insert name" className="input1" id="inp" ref={refName}/>
-                         </div>
-                         <div className="dd">
-                             <input type="text" placeholder="insert surname" className="input1" ref={refSurName}/>
-                         </div>
-                         <div className="dd">
-                             <input type="number" placeholder="insert age" className="input1" ref={refAge}/>
-                         </div>
-                         <input type="submit" className="btn" onClick={addEl}/>
-                     </div>
-                 </div>
-             </div>
-            </body>
-    );
-
-}
-
-export default App
-
-
-
-
-
-
-
+export default App;
--- src/Component/DataTable.js
+++ src/Component/DataTable.js
@@ -1,29 +1,79 @@
-import React from "react";
-import "../App.css"
+import React, { useState } from "react";
+import "../App.css";
 import TableHead from "./TableHead";
 
-const DataTable = (props,key)=>{
+// eslint-disable-next-line no-unused-vars
+const DataTable = (props, key) => {
+  // eslint-disable-next-line no-unused-vars,react/prop-types
+  const [user, setUsers] = useState(props.filteredNames);
 
+  const refName = React.createRef();
+  const refSurName = React.createRef();
+  const refAge = React.createRef();
 
-    return(
-        <table>
+  const addEl = () => {
+    const rName = refName.current.value;
+    const rSurName = refSurName.current.value;
+    const rAge = refAge.current.value;
+    if (rAge === "" || rSurName === "" || rName === "") {
+      alert("try again");
+    } else {
+      setUsers((current) => [
+        ...current,
+        { name: rName, SurName: rSurName, age: rAge },
+      ]);
+      // eslint-disable-next-line react/prop-types
+      props.setFilteredNames((current) => [
+        ...current,
+        { name: rName, SurName: rSurName, age: rAge },
+      ]);
+    }
+    refName.current.value = " ";
+    refSurName.current.value = " ";
+    refAge.current.value = " ";
+  };
 
-            <TableHead  filteredNames={props.filteredNames} setFilteredNames={props.setFilteredNames}/>
-            {
-                props.filteredNames.map((user,index)=>{
-                    return (
-                        <tr key={index}>
-                            <td>{user.name}</td>
-                            <td>{user.SurName}</td>
-                            <td>{user.age}</td>
-                        </tr>
-                    )
-                })
+  return (
+    <table id="myTable">
+      <TableHead
+        filteredNames={props.filteredNames}
+        setFilteredNames={props.setFilteredNames}
+      />
+      {props.filteredNames.map((user, index) => {
+        return (
+          <tr key={index}>
+            <td>{user.name}</td>
+            <td>{user.SurName}</td>
+            <td>{user.age}</td>
+          </tr>
+        );
+      })}
 
-            }
+      <th className="lastRow">
+        <label>
+          insert name
+          <input type="text" id="input" ref={refName} />
+        </label>{" "}
+      </th>
+      <th className="lastRow">
+        <label>
+          insert SurName
+          <input type="text" id="input" ref={refSurName} />
+        </label>{" "}
+      </th>
+      <th className="lastRow">
+        {" "}
+        <label>
+          insert Age <input type="number" id="input" ref={refAge} />
+        </label>
+      </th>
+      <tr>
+        <button className="insertElButton" onClick={addEl}>
+          insert
+        </button>
+      </tr>
+    </table>
+  );
+};
 
-        </table>
-    )
-}
-
-export default DataTable
+export default DataTable;
